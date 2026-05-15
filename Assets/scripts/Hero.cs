9673903a7cbc433ced0c953284d49b4a3f68bbde
Shevaliye <egkomarov@edu.hse.ru>
2026-05-15 21:33:00 +0300

    fix jump animation, add run animation and more

--- Assets/scripts/Hero.cs
+++ Assets/scripts/Hero.cs
@@ -7,68 +7,66 @@ public class Hero : MonoBehaviour
     [SerializeField] private AudioSource jumpSound;
     [SerializeField] private AudioSource bookSound;
 
-    [SerializeField] private float speed = 3f;// speed
-    //[SerializeField] private int lives = 5;
+
+    [SerializeField] private float speed = 3f;
     public float fastspeed = 7f;
     public float realspeed;
-    [SerializeField] private float jumpForce = 0.1f;// force of jumpforce
-    public bool isGrounded = false;
+    [SerializeField] private float jumpForce = 0.1f; // УВЕЛИЧЬТЕ силу прыжка! 0.1f слишком мало
+    public bool isGrounded;
     private float horizontalmove = 0f;
 
+    Transform grounded;
+    public LayerMask layerMask;
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     public Animator anim;
 
-
-
     private void Start()
     {
-
-        gameObject.transform.position = new Vector3(Hero_death.teleport_cords[Hero_death.tracker],Hero_death.teleport_cords[Hero_death.tracker + 1], 0);
-
-        
-        gameObject.transform.position = new Vector3(Hero_death.teleport_cords[Hero_death.tracker],Hero_death.teleport_cords[Hero_death.tracker + 1], 0);
-        //gameObject.transform.position = new Vector3(3, 3, 0);
-
+        gameObject.transform.position = new Vector3(Hero_death.teleport_cords[Hero_death.tracker], Hero_death.teleport_cords[Hero_death.tracker + 1], 0);
+        gameObject.transform.position = new Vector3(Hero_death.teleport_cords[Hero_death.tracker], Hero_death.teleport_cords[Hero_death.tracker + 1], 0);
+        grounded = GameObject.Find(this.name + "/grounded").transform;
+        anim = GetComponent<Animator>();
     }
+
     private void Awake()
     {
-       rb = GetComponent<Rigidbody2D>();
-       sprite = GetComponentInChildren<SpriteRenderer>();
-       anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        
     }
-
 
     private void FixedUpdate()
     {
         CheckGround();
     }
 
-
     private void Update()
     {
         horizontalmove = Input.GetAxisRaw("Horizontal") * speed;
+
         if (Mathf.Abs(horizontalmove) > 0.01)
         {
             Run();
+        }
 
-        }
+        // Управление анимацией бега
         anim.SetFloat("movex", Mathf.Abs(horizontalmove));
-        if (isGrounded && Input.GetButton("Jump"))
+
+        isGrounded = Physics2D.Linecast(transform.position, grounded.position, layerMask);
+
+        anim.SetBool("jump", !isGrounded);
+
+        // Прыжок
+        if (isGrounded && Input.GetButtonDown("Jump")) // Используйте GetButtonDown, а не GetButton
         {
+            anim.SetBool("jump", true);
             Jump();
-            
-        }
-        if (isGrounded == false)
-        {
-            anim.SetBool("jumping", true);
-        }
-        else
-        {
-            anim.SetBool("jumping", false);
         }
     }
+
+
 
 
     private void Run()
@@ -83,6 +81,7 @@ public class Hero : MonoBehaviour
             anim.SetBool("run", false);
             realspeed = speed;
         }
+
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, realspeed * Time.deltaTime);
         sprite.flipX = dir.x < 0.0f;
@@ -90,27 +89,29 @@ public class Hero : MonoBehaviour
 
     private void Jump()
     {
+
+
+        rb.velocity = new Vector2(rb.velocity.x, 0); // Сброс вертикальной скорости для более четкого прыжка
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         jumpSound.Play();
     }
 
     private void CheckGround()
     {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.6f);
+        bool wasGrounded = isGrounded;
         isGrounded = collider.Length > 1;
+
+        // Если приземлились, сбрасываем флаг прыжка прямо здесь
+        anim.SetBool("onGround", isGrounded);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform")
         {
-            // Сохраняем мировую позицию
             Vector3 worldPosition = transform.position;
-            
-            // Прикрепляем к платформе
             this.gameObject.transform.parent = collision.transform;
-            
-            // Восстанавливаем мировую позицию
             transform.position = worldPosition;
         }
     }
@@ -119,24 +120,17 @@ public class Hero : MonoBehaviour
     {
         if (collision.gameObject.tag == "Platform")
         {
-            // Сохраняем мировую позицию
             Vector3 worldPosition = transform.position;
-            
-            // Открепляем от платформы
             this.gameObject.transform.parent = null;
-            
-            // Восстанавливаем мировую позицию
             transform.position = worldPosition;
         }
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("book"))
         {
-            
+            bookSound.Play();
         }
-        bookSound.Play();
     }
-
 }
